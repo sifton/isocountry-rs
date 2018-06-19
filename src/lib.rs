ff4ac7dd15b40fe883a152cb7be310ce02603fc7
Brett <sifton@users.noreply.github.com>
2018-06-19 19:39:20 -0400

    doc comments

--- src/lib.rs
+++ src/lib.rs
@@ -3,264 +3,519 @@ extern crate failure;
 
 use std::fmt::{self, Display};
 
+/// Two-character uppercase ISO 3166-1 strings for each country
 pub mod alpha2;
+/// Three-character uppercase ISO 3166-1 strings for each country
 pub mod alpha3;
+/// English short name ISO 3166-1 strings for each country
 pub mod full;
+/// Unsigned ISO 3166-1 integer IDs for each country
 pub mod numeric;
 
 #[derive(Eq, PartialEq, Ord, PartialOrd, Hash)]
+/// An enumeration of all ISO-3166-1 country codes
 pub enum CountryCode {
+    /// Afghanistan
     AFG,
+    /// Åland Islands
     ALA,
+    /// Albania
     ALB,
+    /// Algeria
     DZA,
+    /// American Samoa
     ASM,
+    /// Andorra
     AND,
+    /// Angola
     AGO,
+    /// Anguilla
     AIA,
+    /// Antarctica
     ATA,
+    /// Antigua and Barbuda
     ATG,
+    /// Argentina
     ARG,
+    /// Armenia
     ARM,
+    /// Aruba
     ABW,
+    /// Australia
     AUS,
+    /// Austria
     AUT,
+    /// Azerbaijan
     AZE,
+    /// Bahamas
     BHS,
+    /// Bahrain
     BHR,
+    /// Bangladesh
     BGD,
+    /// Barbados
     BRB,
+    /// Belarus
     BLR,
+    /// Belgium
     BEL,
+    /// Belize
     BLZ,
+    /// Benin
     BEN,
+    /// Bermuda
     BMU,
+    /// Bhutan
     BTN,
+    /// Bolivia (Plurinational State of)
     BOL,
+    /// Bonaire, Sint Eustatius and Saba
     BES,
+    /// Bosnia and Herzegovina
     BIH,
+    /// Botswana
     BWA,
+    /// Bouvet Island
     BVT,
+    /// Brazil
     BRA,
+    /// British Indian Ocean Territory
     IOT,
+    /// Brunei Darussalam
     BRN,
+    /// Bulgaria
     BGR,
+    /// Burkina Faso
     BFA,
+    /// Burundi
     BDI,
+    /// Cabo Verde
     CPV,
+    /// Cambodia
     KHM,
+    /// Cameroon
     CMR,
+    /// Canada
     CAN,
+    /// Cayman Islands
     CYM,
+    /// Central African Republic
     CAF,
+    /// Chad
     TCD,
+    /// Chile
     CHL,
+    /// China
     CHN,
+    /// Christmas Island
     CXR,
+    /// Cocos (Keeling) Islands
     CCK,
+    /// Colombia
     COL,
+    /// Comoros
     COM,
+    /// Congo
     COG,
+    /// Congo (Democratic Republic of the)
     COD,
+    /// Cook Islands
     COK,
+    /// Costa Rica
     CRI,
+    /// Côte d'Ivoire
     CIV,
+    /// Croatia
     HRV,
+    /// Cuba
     CUB,
+    /// Curaçao
     CUW,
+    /// Cyprus
     CYP,
+    /// Czechia
     CZE,
+    /// Denmark
     DNK,
+    /// Djibouti
     DJI,
+    /// Dominica
     DMA,
+    /// Dominican Republic
     DOM,
+    /// Ecuador
     ECU,
+    /// Egypt
     EGY,
+    /// El Salvador
     SLV,
+    /// Equatorial Guinea
     GNQ,
+    /// Eritrea
     ERI,
+    /// Estonia
     EST,
+    /// Ethiopia
     ETH,
+    /// Falkland Islands (Malvinas)
     FLK,
+    /// Faroe Islands
     FRO,
+    /// Fiji
     FJI,
+    /// Finland
     FIN,
+    /// France
     FRA,
+    /// French Guiana
     GUF,
+    /// French Polynesia
     PYF,
+    /// French Southern Territories
     ATF,
+    /// Gabon
     GAB,
+    /// Gambia
     GMB,
+    /// Georgia
     GEO,
+    /// Germany
     DEU,
+    /// Ghana
     GHA,
+    /// Gibraltar
     GIB,
+    /// Greece
     GRC,
+    /// Greenland
     GRL,
+    /// Grenada
     GRD,
+    /// Guadeloupe
     GLP,
+    /// Guam
     GUM,
+    /// Guatemala
     GTM,
+    /// Guernsey
     GGY,
+    /// Guinea
     GIN,
+    /// Guinea-Bissau
     GNB,
+    /// Guyana
     GUY,
+    /// Haiti
     HTI,
+    /// Heard Island and McDonald Islands
     HMD,
+    /// Holy See
     VAT,
+    /// Honduras
     HND,
+    /// Hong Kong
     HKG,
+    /// Hungary
     HUN,
+    /// Iceland
     ISL,
+    /// India
     IND,
+    /// Indonesia
     IDN,
+    /// Iran (Islamic Republic of)
     IRN,
+    /// Iraq
     IRQ,
+    /// Ireland
     IRL,
+    /// Isle of Man
     IMN,
+    /// Israel
     ISR,
+    /// Italy
     ITA,
+    /// Jamaica
     JAM,
+    /// Japan
     JPN,
+    /// Jersey
     JEY,
+    /// Jordan
     JOR,
+    /// Kazakhstan
     KAZ,
+    /// Kenya
     KEN,
+    /// Kiribati
     KIR,
+    /// Korea (Democratic People's Republic of)
     PRK,
+    /// Korea (Republic of)
     KOR,
+    /// Kuwait
     KWT,
+    /// Kyrgyzstan
     KGZ,
+    /// Lao People's Democratic Republic
     LAO,
+    /// Latvia
     LVA,
+    /// Lebanon
     LBN,
+    /// Lesotho
     LSO,
+    /// Liberia
     LBR,
+    /// Libya
     LBY,
+    /// Liechtenstein
     LIE,
+    /// Lithuania
     LTU,
+    /// Luxembourg
     LUX,
+    /// Macao
     MAC,
+    /// Macedonia (the former Yugoslav Republic of)
     MKD,
+    /// Madagascar
     MDG,
+    /// Malawi
     MWI,
+    /// Malaysia
     MYS,
+    /// Maldives
     MDV,
+    /// Mali
     MLI,
+    /// Malta
     MLT,
+    /// Marshall Islands
     MHL,
+    /// Martinique
     MTQ,
+    /// Mauritania
     MRT,
+    /// Mauritius
     MUS,
+    /// Mayotte
     MYT,
+    /// Mexico
     MEX,
+    /// Micronesia (Federated States of)
     FSM,
+    /// Moldova (Republic of)
     MDA,
+    /// Monaco
     MCO,
+    /// Mongolia
     MNG,
+    /// Montenegro
     MNE,
+    /// Montserrat
     MSR,
+    /// Morocco
     MAR,
+    /// Mozambique
     MOZ,
+    /// Myanmar
     MMR,
+    /// Namibia
     NAM,
+    /// Nauru
     NRU,
+    /// Nepal
     NPL,
+    /// Netherlands
     NLD,
+    /// New Caledonia
     NCL,
+    /// New Zealand
     NZL,
+    /// Nicaragua
     NIC,
+    /// Niger
     NER,
+    /// Nigeria
     NGA,
+    /// Niue
     NIU,
+    /// Norfolk Island
     NFK,
+    /// Northern Mariana Islands
     MNP,
+    /// Norway
     NOR,
+    /// Oman
     OMN,
+    /// Pakistan
     PAK,
+    /// Palau
     PLW,
+    /// Palestine, State of
     PSE,
+    /// Panama
     PAN,
+    /// Papua New Guinea
     PNG,
+    /// Paraguay
     PRY,
+    /// Peru
     PER,
+    /// Philippines
     PHL,
+    /// Pitcairn
     PCN,
+    /// Poland
     POL,
+    /// Portugal
     PRT,
+    /// Puerto Rico
     PRI,
+    /// Qatar
     QAT,
+    /// Réunion
     REU,
+    /// Romania
     ROU,
+    /// Russian Federation
     RUS,
+    /// Rwanda
     RWA,
+    /// Saint Barthélemy
     BLM,
+    /// Saint Helena, Ascension and Tristan da Cunha
     SHN,
+    /// Saint Kitts and Nevis
     KNA,
+    /// Saint Lucia
     LCA,
+    /// Saint Martin (French part)
     MAF,
+    /// Saint Pierre and Miquelon
     SPM,
+    /// Saint Vincent and the Grenadines
     VCT,
+    /// Samoa
     WSM,
+    /// San Marino
     SMR,
+    /// Sao Tome and Principe
     STP,
+    /// Saudi Arabia
     SAU,
+    /// Senegal
     SEN,
+    /// Serbia
     SRB,
+    /// Seychelles
     SYC,
+    /// Sierra Leone
     SLE,
+    /// Singapore
     SGP,
+    /// Sint Maarten (Dutch part)
     SXM,
+    /// Slovakia
     SVK,
+    /// Slovenia
     SVN,
+    /// Solomon Islands
     SLB,
+    /// Somalia
     SOM,
+    /// South Africa
     ZAF,
+    /// South Georgia and the South Sandwich Islands
     SGS,
+    /// South Sudan
     SSD,
+    /// Spain
     ESP,
+    /// Sri Lanka
     LKA,
+    /// Sudan
     SDN,
+    /// Suriname
     SUR,
+    /// Svalbard and Jan Mayen
     SJM,
+    /// Swaziland
     SWZ,
+    /// Sweden
     SWE,
+    /// Switzerland
     CHE,
+    /// Syrian Arab Republic
     SYR,
+    /// Taiwan, Province of China
     TWN,
+    /// Tajikistan
     TJK,
+    /// Tanzania, United Republic of
     TZA,
+    /// Thailand
     THA,
+    /// Timor-Leste
     TLS,
+    /// Togo
     TGO,
+    /// Tokelau
     TKL,
+    /// Tonga
     TON,
+    /// Trinidad and Tobago
     TTO,
+    /// Tunisia
     TUN,
+    /// Turkey
     TUR,
+    /// Turkmenistan
     TKM,
+    /// Turks and Caicos Islands
     TCA,
+    /// Tuvalu
     TUV,
+    /// Uganda
     UGA,
+    /// Ukraine
     UKR,
+    /// United Arab Emirates
     ARE,
+    /// United Kingdom of Great Britain and Northern Ireland
     GBR,
+    /// United States of America
     USA,
+    /// United States Minor Outlying Islands
     UMI,
+    /// Uruguay
     URY,
+    /// Uzbekistan
     UZB,
+    /// Vanuatu
     VUT,
+    /// Venezuela (Bolivarian Republic of)
     VEN,
+    /// Viet Nam
     VNM,
+    /// Virgin Islands (British)
     VGB,
+    /// Virgin Islands (U.S.)
     VIR,
+    /// Wallis and Futuna
     WLF,
+    /// Western Sahara
     ESH,
+    /// Yemen
     YEM,
+    /// Zambia
     ZMB,
+    /// Zimbabwe
     ZWE,
 }
 
+/// Errors that might arise when converting raw data into CountryCode
 #[derive(Debug, Fail, PartialEq)]
 pub enum CountryCodeParseErr {
     #[fail(display = "invalid alpha-2 country code string: {}", unknown)]
@@ -272,6 +527,7 @@ pub enum CountryCodeParseErr {
 }
 
 impl CountryCode {
+    /// Returns the ISO 3166-1 English short name
     pub fn full_name(&self) -> &'static str {
         use self::CountryCode::*;
         use full::*;
@@ -528,7 +784,8 @@ impl CountryCode {
         }
     }
 
-    fn numeric_id(&self) -> u32 {
+    /// Returns the ISO 3166-1 numeric ID
+    pub fn numeric_id(&self) -> u32 {
         use self::CountryCode::*;
         use numeric::*;
         match *self {
@@ -784,6 +1041,7 @@ impl CountryCode {
         }
     }
 
+    /// Returns the uppercase two-character ISO 3166-1 string
     pub fn alpha2(&self) -> &'static str {
         use self::CountryCode::*;
         use alpha2::*;
@@ -1040,6 +1298,7 @@ impl CountryCode {
         }
     }
 
+    /// Returns the uppercase three-character ISO 3166-1 string
     pub fn alpha3(&self) -> &'static str {
         use self::CountryCode::*;
         use alpha3::*;
@@ -1296,13 +1555,12 @@ impl CountryCode {
         }
     }
 
-    pub fn parse2<T>(value: T) -> Result<CountryCode, CountryCodeParseErr>
-    where
-        T: AsRef<str>,
-    {
+    /// Attempts to determine the ISO 3166-1 CountryCode for the given two-character string,
+    /// assuming it is in upper-case characters
+    pub fn for_alpha2(value: &str) -> Result<CountryCode, CountryCodeParseErr> {
         use CountryCode::*;
         use alpha2::*;
-        match value.as_ref() {
+        match value {
             ISO_A2_AFG => Ok(AFG),
             ISO_A2_ALA => Ok(ALA),
             ISO_A2_ALB => Ok(ALB),
@@ -1558,13 +1816,19 @@ impl CountryCode {
         }
     }
 
-    pub fn parse3<T>(value: T) -> Result<CountryCode, CountryCodeParseErr>
-    where
-        T: AsRef<str>,
-    {
+    /// Attempts to determine the ISO 3166-1 CountryCode for the given two-character string,
+    /// making no assumption as to the case-ness of the input
+    pub fn for_alpha2_caseless(value: &str) -> Result<CountryCode, CountryCodeParseErr> {
+        let uppercase = value.to_uppercase();
+        CountryCode::for_alpha2(uppercase.as_ref())
+    }
+
+    /// Attempts to determine the ISO 3166-1 CountryCode for the given three-character string,
+    /// assuming it is in upper-case characters
+    pub fn for_alpha3(value: &str) -> Result<CountryCode, CountryCodeParseErr> {
         use CountryCode::*;
         use alpha2::*;
-        match value.as_ref() {
+        match value {
             ISO_A3_AFG => Ok(AFG),
             ISO_A3_ALA => Ok(ALA),
             ISO_A3_ALB => Ok(ALB),
@@ -1820,6 +2084,14 @@ impl CountryCode {
         }
     }
 
+    /// Attempts to determine the ISO 3166-1 CountryCode for the given three-character string,
+    /// making no assumption as to the case-ness of the input
+    pub fn for_alpha3_caseless(value: &str) -> Result<CountryCode, CountryCodeParseErr> {
+        let uppercase = value.to_uppercase();
+        CountryCode::for_alpha3(uppercase.as_ref())
+    }
+
+    /// Attempts to determine the ISO 3166-1 CountryCode for the given unsigned integer
     pub fn for_id(value: u32) -> Result<CountryCode, CountryCodeParseErr> {
         use CountryCode::*;
         use numeric::*;
